4cdcee78a67523923a6e8ecd0b0018d7be8e6dd4
Lisa Dean <lisa@lisadean.net>
2015-04-15 22:55:22 -0400

    Created program

---
+++ StudentInformation.java
@@ -0,0 +1,14 @@
+/*
+StudentInformation.java
+
+Program description
+*/
+
+public class StudentInformation
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Hello world");
+		System.out.println();
+	}
+}
